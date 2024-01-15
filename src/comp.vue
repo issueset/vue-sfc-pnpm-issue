0add1fb0b336d7b91a583cee7c26e605f502c233
ocavue <ocavue@gmail.com>
2024-01-16 03:25:47 +0800

    init

---
+++ src/comp.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import type { VueNodeViewProps } from 'prosekit/vue'
+
+const props = defineProps<VueNodeViewProps>()
+</script>
+
+<template>
+  <pre>{{ props.node }}</pre>
+</template>
---
+++ src/main.ts
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import Comp from './comp.vue'
+
+createApp(Comp).mount('#app')
